6b7ee166952b40b21e61197923bdd4ba464fe404
skydoves <skydoves@naver.com>
2017-06-29 00:44:33 +0900

    initialize

---
+++ RurimoCamera/MainForm.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace RurimoCamera
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다.
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마십시오.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.nammingbox = new System.Windows.Forms.ToolStripTextBox();
+            this.toolStripLabel3 = new System.Windows.Forms.ToolStripLabel();
+            this.sizelabel = new System.Windows.Forms.ToolStripLabel();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1,
+            this.toolStripButton2,
+            this.toolStripSeparator1,
+            this.toolStripLabel1,
+            this.nammingbox,
+            this.toolStripLabel3,
+            this.sizelabel});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(523, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton1.Image = global::RurimoCamera.Properties.Resources.Camera;
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton1.Text = "스크린샷 : F2";
+            this.toolStripButton1.Click += new System.EventHandler(this.toolStripButton1_Click);
+            // 
+            // toolStripButton2
+            // 
+            this.toolStripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton2.Image = global::RurimoCamera.Properties.Resources.normal_folder;
+            this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton2.Name = "toolStripButton2";
+            this.toolStripButton2.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton2.Text = "폴더 정하기";
+            this.toolStripButton2.Click += new System.EventHandler(this.toolStripButton2_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 25);
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(54, 22);
+            this.toolStripLabel1.Text = "네이밍 : ";
+            // 
+            // nammingbox
+            // 
+            this.nammingbox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.nammingbox.Name = "nammingbox";
+            this.nammingbox.Size = new System.Drawing.Size(100, 25);
+            this.nammingbox.Text = "screenshot";
+            // 
+            // toolStripLabel3
+            // 
+            this.toolStripLabel3.Name = "toolStripLabel3";
+            this.toolStripLabel3.Size = new System.Drawing.Size(23, 22);
+            this.toolStripLabel3.Text = "    ";
+            // 
+            // sizelabel
+            // 
+            this.sizelabel.Name = "sizelabel";
+            this.sizelabel.Size = new System.Drawing.Size(89, 22);
+            this.sizelabel.Text = "width : height :";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(523, 346);
+            this.Controls.Add(this.toolStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(120, 85);
+            this.Name = "MainForm";
+            this.Text = "Rurimo Camera v1.3 by skydoves";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.Resize += new System.EventHandler(this.FormResize);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.ToolStripButton toolStripButton2;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+        private System.Windows.Forms.ToolStripTextBox nammingbox;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripLabel sizelabel;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel3;
+
+    }
+}
+
---
+++ RurimoCamera/MainForm.cs
@@ -0,0 +1,266 @@
+﻿
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.IO;
+using System.Drawing.Imaging;
+
+namespace RurimoCamera
+{
+    public partial class MainForm : Form
+    {
+        public static string FolderPath = "";
+        public static MainForm mainform;
+
+        // Keyboard Hook
+        private const int WH_KEYBOARD_LL = 13;
+        private const int WM_KEYDOWN = 0x0100;
+        private static LowLevelKeyboardProc _proc = HookCallback;
+        private static IntPtr _hookID = IntPtr.Zero;   
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        // MainForm Load
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+          // Form Transparent
+          this.TransparencyKey = Color.Turquoise;
+          this.BackColor = Color.Turquoise;
+
+          mainform = this;
+          FolderPath = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+          sizelabel.Text = "width : " + (this.Width - 16) + " height : " + (this.Height - 64);
+
+           // Global Keyboard hooking
+          _hookID = SetHook(_proc);
+        }
+
+        // ScreenShot Menu Button
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            // screenShot
+            ScreenShot();
+        }
+
+
+        /**
+         * @ScreenShot
+         * geting form size and saving a image as ".png" file with random naming
+         */
+        private void ScreenShot()
+        {
+            this.WindowState = FormWindowState.Normal;
+            Bitmap cropped = null;
+
+            var bmpScreenshot = new Bitmap(Screen.PrimaryScreen.Bounds.Width,
+                               Screen.PrimaryScreen.Bounds.Height,
+                               PixelFormat.Format32bppArgb);
+
+            var gfxScreenshot = Graphics.FromImage(bmpScreenshot);
+            gfxScreenshot.CopyFromScreen(Screen.PrimaryScreen.Bounds.X,
+                                        Screen.PrimaryScreen.Bounds.Y,
+                                        0,
+                                        0,
+                                        Screen.PrimaryScreen.Bounds.Size,
+                                        CopyPixelOperation.SourceCopy);
+
+            // do screenShot
+            string random = ((new Random()).Next()).ToString(); // set random file name
+            if (Directory.Exists(FolderPath))
+            {
+                try
+                {
+                    Rectangle rect = new Rectangle(this.Location.X + 8, this.Location.Y + 56, this.Width - 16, this.Height - 64);
+                    cropped = (Bitmap)bmpScreenshot.Clone(rect, bmpScreenshot.PixelFormat);
+                    cropped.Save(FolderPath + "\\" + nammingbox.Text + random + ".png", ImageFormat.Png);
+                }
+                catch
+                {
+                    try
+                    {
+                        // Determine the size of the "virtual screen", which includes all monitors.
+                        int screenLeft = SystemInformation.VirtualScreen.Left;
+                        int screenTop = SystemInformation.VirtualScreen.Top;
+                        int screenWidth = SystemInformation.VirtualScreen.Width;
+                        int screenHeight = SystemInformation.VirtualScreen.Height;
+
+                        // Create a bitmap of the appropriate size to receive the screenshot.
+                        using (Bitmap bmp = new Bitmap(screenWidth, screenHeight))
+                        {
+                            // Draw the screenshot into our bitmap.
+                            using (Graphics g = Graphics.FromImage(bmp))
+                            {
+                                g.CopyFromScreen(screenLeft, screenTop, 0, 0, bmp.Size);
+                            }
+
+                            Rectangle rect = new Rectangle(this.Location.X + 8, this.Location.Y + 56, this.Width - 16, this.Height - 64);
+                            cropped = (Bitmap)bmp.Clone(rect, bmp.PixelFormat);
+
+                            // Do something with the Bitmap here, like save it to a file:
+                            cropped.Save(FolderPath + "\\" + nammingbox.Text + random + ".png", ImageFormat.Png);
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        // Show Window Message
+                        IntPtr handle = IntPtr.Zero;
+                        handle = GetForegroundWindow();
+                        MessageBox.Show(new WindowWrapper(handle), "Check camera's position", "Rurimo Camera");
+                    }
+                }
+            }
+            else
+            {
+                // Show Window Message
+                IntPtr handle = IntPtr.Zero;
+                handle = GetForegroundWindow();
+                MessageBox.Show(new WindowWrapper(handle), "The specified folder does not exist", "Rurimo Camera");
+            }
+        }
+
+
+        /**
+         * @ScreenClipboard
+         * geting form size and saving a image at Clipboard
+         */
+        private Bitmap ScreenClipboard()
+        {
+            Bitmap cropped = null;
+
+            // Create Random Directory
+            string random = ((new Random()).Next()).ToString();
+
+            if (Directory.Exists(FolderPath))
+            {
+                try
+                {
+                    // Determine the size of the "virtual screen", which includes all monitors.
+                    int screenLeft = SystemInformation.VirtualScreen.Left;
+                    int screenTop = SystemInformation.VirtualScreen.Top;
+                    int screenWidth = SystemInformation.VirtualScreen.Width;
+                    int screenHeight = SystemInformation.VirtualScreen.Height;
+
+                    // Create a bitmap of the appropriate size to receive the screenshot.
+                    using (Bitmap bmp = new Bitmap(screenWidth, screenHeight))
+                    {
+                        // Draw the screenshot into our bitmap.
+                        using (Graphics g = Graphics.FromImage(bmp))
+                        {
+                            g.CopyFromScreen(screenLeft, screenTop, 0, 0, bmp.Size);
+                        }
+
+                        Rectangle rect = new Rectangle(this.Location.X + 8, this.Location.Y + 56, this.Width - 16, this.Height - 64);
+                        cropped = (Bitmap)bmp.Clone(rect, bmp.PixelFormat);
+
+                    }
+                }
+                catch (Exception e)
+                {
+                    // Show Window Message
+                    IntPtr handle = IntPtr.Zero;
+                    handle = GetForegroundWindow();
+                    MessageBox.Show(new WindowWrapper(handle), "Check camera's position", "Rurimo Camera");
+                }
+            }
+            else
+            {
+                // Show Window Message
+                IntPtr handle = IntPtr.Zero;
+                handle = GetForegroundWindow();
+                MessageBox.Show(new WindowWrapper(handle), "The specified folder does not exist", "Rurimo Camera");
+            }
+
+            return cropped;
+        }
+
+        #region Global Keyboard Hoook
+
+        // Set Hook
+        private static IntPtr SetHook(LowLevelKeyboardProc proc)
+        {
+            using (Process curProcess = Process.GetCurrentProcess())
+            using (ProcessModule curModule = curProcess.MainModule)
+            {
+                return SetWindowsHookEx(WH_KEYBOARD_LL, proc,
+                GetModuleHandle(curModule.ModuleName), 0);
+            }
+        }
+
+        private delegate IntPtr LowLevelKeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);
+        private static IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            if (nCode >= 0 && wParam == (IntPtr)WM_KEYDOWN)
+            {
+                int vkCode = Marshal.ReadInt32(lParam);
+
+                // F2 : Screen Shot
+
+                if ((((Keys)vkCode).ToString()).Equals("F2"))
+                {
+                    mainform.ScreenShot();
+                }
+
+                else if ((((Keys)vkCode).ToString()).Equals("F3"))
+                {
+                    Clipboard.SetImage(mainform.ScreenClipboard());
+                }
+
+            }
+
+            return CallNextHookEx(_hookID, nCode, wParam, lParam);
+        }
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr SetWindowsHookEx(int idHook, LowLevelKeyboardProc lpfn,
+                            IntPtr hMod, uint dwThreadId);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode,
+                            IntPtr wParam, IntPtr lParam);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr GetModuleHandle(string lpModuleName);
+
+        #endregion
+
+
+        // Select Folder
+        private void toolStripButton2_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() != DialogResult.Cancel)
+                FolderPath = folderBrowserDialog1.SelectedPath;
+        }
+
+        private void FormResize(object sender, EventArgs e)
+        {
+            sizelabel.Text = "width : " + (this.Width - 16) + " height : " + (this.Height - 64);
+        }
+
+        [DllImport("user32")]
+        private static extern IntPtr GetForegroundWindow();
+
+        // Win32 Api
+        public class WindowWrapper : System.Windows.Forms.IWin32Window
+        {
+            private IntPtr _hwnd;
+            public WindowWrapper(IntPtr handle)
+            {
+                _hwnd = handle;
+            }
+            public IntPtr Handle
+            {
+                get { return _hwnd; }
+            }
+        }
+
+    }
+}
